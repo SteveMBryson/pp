7c18463b6aeca10ca4b64549c4f3541a34c4d713
BRYSN <stephenbryson@brysn.uk>
2018-04-12 16:01:49 +0100

    Script for opting out

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-// Opts-out of all services
-function optOutAllServices(){
-  $('#opt-out').prop('checked', false).click(function() {
-    $('.service-checkbox').prop('checked', false);
+$( document ).ready(function() {
+
+  // Opts-out of all services
+  function optOutAllServices(){
+    $('#opt-out').prop('checked', false).click(function() {
+      $('.option').prop('checked', false);
+    });
+  }
+
+  // Un-checks the opt out box if any input is ticked
+  $('.option').prop('checked', false).click(function() {
+    $('#opt-out').prop('checked', false);
   });
-}
 
-// Un-checks the opt out box if any input is ticked
-$('.service-checkbox').prop('checked', false).click(function() {
-  $('#opt-out').prop('checked', false);
+  optOutAllServices();
+
+  // Set the pre-checked services
+  $('.option[checked]').prop('checked', true);
+
 });
-
-optOutAllServices();
-
-// Set the pre-checked services
-$('.service-checkbox[checked]').prop('checked', true);

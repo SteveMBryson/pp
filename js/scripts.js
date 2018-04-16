b2815b6b54ae3c64d8e6667023c1a458a3019e86
BRYSN <stephenbryson@brysn.uk>
2018-04-16 11:50:45 +0100

    removed junk script

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,4 @@ $( document ).ready(function() {
     $('.service-options__option input').toggleDisabled();
   });
 
-
-  var tooltip = document.querySelectorAll('.service-options__tooltip');
-
-  document.addEventListener('mousemove', fn, false);
-
-  function fn(e) {
-      for (var i=tooltip.length; i--;) {
-          tooltip[i].style.left = e.pageX + 'px';
-          tooltip[i].style.top = e.pageY + 'px';
-      }
-  }
-
 });
